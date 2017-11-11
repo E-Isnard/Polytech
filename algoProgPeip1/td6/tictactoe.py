5595e7325051048625825b57bc6ca2f86042d4c8
enzo-X200 <enzoisnard@gmail.com>
2017-11-11 13:27:40 +0100

    Titactoe: ajout d'une fonction qui dessine une grille

---
+++ algoProgPeip1/td6/tictactoe.py
@@ -0,0 +1,26 @@
+from turtle import *
+import time
+
+hideturtle()
+speed("fastest")
+def drawSquare(x,y):
+    up()
+    goto(x,y)
+    down()
+    for i in range(4):
+        forward(150)
+        left(90)
+
+def drawGrid():
+    c = 0
+    l = 0
+    for i in range(3):
+        for j in range(3):
+            drawSquare(-200+l,-200+c)
+            l += 150
+        l = 0
+        c += 150
+
+
+
+drawGrid()
