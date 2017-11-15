36fe37b4d37ff7151439a3f904ec5bd7b12d63e0
enzo-X200 <enzoisnard@gmail.com>
2017-11-15 18:21:56 +0100

    Rajout de la fonction "play"

--- algoProgPeip1/td6/tictactoe.py
+++ algoProgPeip1/td6/tictactoe.py
@@ -1,7 +1,6 @@
 from turtle import *
 import time
-
-hideturtle()
+#hideturtle()
 speed("fastest")
 def drawSquare(x,y):
     up()
@@ -10,6 +9,12 @@ def drawSquare(x,y):
     for i in range(4):
         forward(150)
         left(90)
+
+def drawCircle(x,y):
+    up()
+    goto(x,y)
+    down()
+    circle(75)
 
 def drawGrid():
     c = 0
@@ -20,8 +25,37 @@ def drawGrid():
             l += 150
         l = 0
         c += 150
+        
+
+def play(x,y):
+
+    if -200<x<-50 and 100<y<250:
+            drawCircle(-125,100)
+    if -50<x<100 and 100<y<250:
+            drawCircle(25,100)
+    if 100<x<250 and 100<y<250:
+            drawCircle(175,100)
+
+    if -200<x<-50 and -50<y<100:
+            drawCircle(-125,-50)
+    if -50<x<100 and -50<y<100:
+            drawCircle(25,-50)
+    if 100<x<250 and -50<y<100:
+            drawCircle(175,-50)
+
+    if -200<x<-50 and -200<y<-50:
+            drawCircle(-125,-200)
+    if -50<x<100 and -200<y<-50:
+            drawCircle(25,-200)
+    if 100<x<250 and -200<y<-50:
+            drawCircle(175,-200)
+
+
+
+
 
 
 
 drawGrid()
+onscreenclick(play)
 mainloop()
