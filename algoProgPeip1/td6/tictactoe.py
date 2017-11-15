d5356167a6e09151a07e951df1cabb988b85888f
enzo-X200 <enzoisnard@gmail.com>
2017-11-15 23:56:12 +0100

    Ajout du joueur 2

--- algoProgPeip1/td6/tictactoe.py
+++ algoProgPeip1/td6/tictactoe.py
@@ -1,13 +1,15 @@
 from turtle import *
+from math import *
 import time
-hideturtle()
+#hideturtle()
+player = 1
 speed("fastest")
-def drawSquare(x,y):
+def drawSquare(x,y,l):
     up()
     goto(x,y)
     down()
     for i in range(4):
-        forward(150)
+        forward(l)
         left(90)
 
 def drawCircle(x,y):
@@ -21,14 +23,14 @@ def drawGrid():
     l = 0
     for i in range(3):
         for j in range(3):
-            drawSquare(-200+l,-200+c)
+            drawSquare(-200+l,-200+c,150)
             l += 150
         l = 0
         c += 150
         
 
-def play(x,y):
-
+def player1(x,y):
+    global player    
     if -200<x<-50 and 100<y<250:
             drawCircle(-125,100)
     if -50<x<100 and 100<y<250:
@@ -49,7 +51,37 @@ def play(x,y):
             drawCircle(25,-200)
     if 100<x<250 and -200<y<-50:
             drawCircle(175,-200)
+    
+    player = 2    
 
+def player2(x,y):
+    global player    
+    seth(45)    
+    if -200<x<-50 and 100<y<250:
+            drawSquare(-125,100,sqrt(2*75**2))
+    if -50<x<100 and 100<y<250:
+            drawSquare(25,100,sqrt(2*75**2))
+    if 100<x<250 and 100<y<250:
+            drawSquare(175,100,sqrt(2*75**2))
+
+    if -200<x<-50 and -50<y<100:
+            drawSquare(-125,-50,sqrt(2*75**2))
+    if -50<x<100 and -50<y<100:
+            drawSquare(25,-50,sqrt(2*75**2))
+    if 100<x<250 and -50<y<100:
+            drawSquare(175,-50,sqrt(2*75**2))
+
+    if -200<x<-50 and -200<y<-50:
+            drawSquare(-125,-200,sqrt(2*75**2))
+    if -50<x<100 and -200<y<-50:
+            drawSquare(25,-200,sqrt(2*75**2))
+    if 100<x<250 and -200<y<-50:
+            drawSquare(175,-200,sqrt(2*75**2))
+    
+    player = 2      
+        
 drawGrid()
-onscreenclick(play)
+
+
+
 mainloop()
