4c4115d9df57643381b4131b1e548f3e2c495153
enzo-X200 <enzoisnard@gmail.com>
2017-11-20 21:36:44 +0100

    ajout d'une liste grid

--- algoProgPeip1/td6/tictactoe.py
+++ algoProgPeip1/td6/tictactoe.py
@@ -4,6 +4,7 @@ import time
 hideturtle()
 player = 1
 speed("fastest")
+grid = [[0,0,0],[0,0,0],[0,0,0]]
 def drawSquare(x,y,l):
         up()
         goto(x,y)
@@ -33,24 +34,33 @@ def player1(x,y):
         global player
         if -200<x<-50 and 100<y<250:
                 drawCircle(-125,100)
+                grid[0].insert(0,1)
         if -50<x<100 and 100<y<250:
                 drawCircle(25,100)
+                grid[0].insert(1,1)
         if 100<x<250 and 100<y<250:
                 drawCircle(175,100)
+                grid[0].insert(2,1)
 
         if -200<x<-50 and -50<y<100:
                 drawCircle(-125,-50)
+                grid[1].insert(0,1)
         if -50<x<100 and -50<y<100:
                 drawCircle(25,-50)
+                grid[1].insert(1,1)
         if 100<x<250 and -50<y<100:
                 drawCircle(175,-50)
+                grid[1].insert(2,1)
 
         if -200<x<-50 and -200<y<-50:
                 drawCircle(-125,-200)
+                grid[2].insert(0,1)
         if -50<x<100 and -200<y<-50:
                 drawCircle(25,-200)
+                grid[2].insert(1,1)
         if 100<x<250 and -200<y<-50:
                 drawCircle(175,-200)
+                grid[2].insert(2,1)
     
         player = 2
 
@@ -59,32 +69,44 @@ def player2(x,y):
         seth(45)    
         if -200<x<-50 and 100<y<250:
                 drawSquare(-125,100,sqrt(2*75**2))
+                grid[0].insert(0,2)
         if -50<x<100 and 100<y<250:
                 drawSquare(25,100,sqrt(2*75**2))
+                grid[0].insert(1,2)
         if 100<x<250 and 100<y<250:
                 drawSquare(175,100,sqrt(2*75**2))
+                grid[0].insert(2,2)
 
         if -200<x<-50 and -50<y<100:
                 drawSquare(-125,-50,sqrt(2*75**2))
+                grid[1].insert(0,2)
         if -50<x<100 and -50<y<100:
                 drawSquare(25,-50,sqrt(2*75**2))
+                grid[1].insert(1,2)
         if 100<x<250 and -50<y<100:
                 drawSquare(175,-50,sqrt(2*75**2))
-
+                grid[1].insert(2,2)
+        
         if -200<x<-50 and -200<y<-50:
                 drawSquare(-125,-200,sqrt(2*75**2))
+                grid[2].insert(0,2)
         if -50<x<100 and -200<y<-50:
                 drawSquare(25,-200,sqrt(2*75**2))
+                grid[2].insert(1,2)
         if 100<x<250 and -200<y<-50:
-                drawSquare(175,-200,sqrt(2*75**2))   
+                drawSquare(175,-200,sqrt(2*75**2))
+                grid[2].insert(2,2)   
         seth(0)
         player = 1
+
+
 
 def turn(x,y):
         if player == 1:
                 player1(x,y)
         elif player == 2:
                 player2(x,y)
+
 
 drawGrid()
 
