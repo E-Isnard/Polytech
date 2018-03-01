633848f9997ab92a2dff31e6e098bccb267f5d1f
Enzo Isnard <enzoisnard@gmail.com>
2018-03-01 11:49:52 +0100

    Mise à jour de ls.py

--- TDInfoISNARDEnzo/td2/ls.py
+++ TDInfoISNARDEnzo/td2/ls.py
@@ -3,14 +3,24 @@ from peip3 import *
 
 def main():
 	if len(argv) == 1:
-		arg = "*"
+		for file in direntries("*"):
+			print(file)
+	if len(argv) == 2:
+		if not(exists(argv[1])) and not(isdir(argv[1])):
+			print(argv[1] +":Votre argument n'est pas valide >:c\n")
+		else:
+			for file in direntries(argv[1] + "/*"):
+				print(basename(file))
 	else:
-		arg = "/".join(argv[1:])
-		arg = arg + "/*" 	
-	for file in direntries(arg):
-		print(basename(file))
+		for arg in argv[1:]:
+			if not(exists(arg)) and not(isdir(arg)):
+				print(arg + ": Cet argument n'est pas valide >:c\n")
+			else:
+				print(arg+":")
+				for file in direntries(arg + "/*"):
+					print(basename(file))
+		
 	
-main()
-	
+main()	
 	  
 
