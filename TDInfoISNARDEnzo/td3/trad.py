44d9aba086770c07d2c91d1f424b0dea10c50954
Enzo Isnard <enzoisnard@gmail.com>
2018-03-14 20:20:29 +0100

    .

--- TDInfoISNARDEnzo/td3/trad.py
+++ TDInfoISNARDEnzo/td3/trad.py
@@ -2,16 +2,26 @@
 from peip3 import *
 
 def replace(f,entree,sortie):
-	for line in f:
-		if regsearch(entree,line):
-			print(regsub(entree,sortie,line),end='')
-			
-			 
+    for line in f:
+        if regsearch(entree,line):
+            print(regsub(entree,sortie,line),end='')
+            
+             
 
 def url_builder(string):
-	out = regsearch(" ","%20",string)
-	
+    out = regsub(" ","%20",string)
+    out = "https://translate.google.fr/#en/fr/it%20rains"+out
+    return out
+
+def translate(url):
+    page = urlopen(url)
+
+    for line in page:
+        if regsearch("TRANSLATED_TEXT",line):
+            print(line)
+    
 
 def main():
-	
+    translate(url_builder("oui oui oui"))
+    
 main()
