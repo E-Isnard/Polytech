38fdb02708c588d0036723e1b5e9b30a0fe8919e
Enzo Isnard <enzoisnard@gmail.com>
2018-03-25 14:47:37 +0200

    Mise à jour de trad.py

--- TDInfoISNARDEnzo/td3/trad.py
+++ TDInfoISNARDEnzo/td3/trad.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 from peip3 import *
 
-def replace(f,entree,sortie):
-    for line in f:
-        if regsearch(entree,line):
-            print(regsub(entree,sortie,line),end='')
-            
-             
-
 def url_builder(string):
     out = regsub(" ","%20",string)
-    out = "https://translate.google.fr/#en/fr/it%20rains"+out
+    out = "http://translate.google.com/translate_t?text="+out+"&langpair=en|fr"
     return out
 
 def translate(url):
     page = urlopen(url)
-
+    pattern = "^.*;TRANSLATED_TEXT='([^']*)';.*$"
     for line in page:
         if regsearch("TRANSLATED_TEXT",line):
-            print(line)
-    
+            return regsub(pattern,"\\1",line)
 
 def main():
-    translate(url_builder("oui oui oui"))
-    
+    print(translate(url_builder(input("Quelle phrase voulez-vous traduire?:\n"))))
+
 main()
