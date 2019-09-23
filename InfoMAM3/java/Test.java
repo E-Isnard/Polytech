14af122f7fd00d1ad4c0d0c87effa37294ba5703
Isnard <enzoisnard@gmail.com>
2019-09-23 11:04:43 +0200

    Oui

--- InfoMAM3/java/Test.java
+++ InfoMAM3/java/Test.java
@@ -6,8 +6,15 @@ public class Test {
         // oui.insertMoney(200);
         // System.out.println(oui.getTicket());
 
-        Student s = new Student("jde","jdjdslskde","21212");
-        System.out.println(s.getLogin());
+        // Student s = new Student("jde","jdjdslskde","21212");
+        // System.out.println(s.getLogin());
+
+        ClockDisplay cd = new ClockDisplay();
+        cd.timeTick();
+        System.out.println(cd.getTime());
+        cd.setTime(15, 0);
+        System.out.println(cd.getTime());
+        
 
 
     }
