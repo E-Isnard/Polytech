380245222a705228d66e47f8bd3596d3fd90e8e7
Isnard <enzoisnard@gmail.com>
2020-01-23 13:05:34 +0100

    Ajout de l'option -q au script

--- ProjetPython/compressionImage.py
+++ ProjetPython/compressionImage.py
@@ -12,9 +12,10 @@ for i in range(8):
             P[i, j] = np.sqrt(1/4)*np.cos((np.pi*(2*j+1)*i)/16)
 
 
-Q = np.matrix('16 11 10 16 24 40 51 61;12 12 13 19 26 58 60 55;14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62;18 22 37 56 68 109 103 77;24 35 55 64 81 104 113 92;49 64 78 87 103 121 120 101;72 92 95 98 112 100 103 99')
+# Q = np.matrix('16 11 10 16 24 40 51 61;12 12 13 19 26 58 60 55;14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62;18 22 37 56 68 109 103 77;24 35 55 64 81 104 113 92;49 64 78 87 103 121 120 101;72 92 95 98 112 100 103 99')
 # Q=np.ones((8,8))*255
 # Q[0,0]=1
+
 
 def tronquer(image):
     """
@@ -26,14 +27,16 @@ def tronquer(image):
     newWidth = width - width % 8
     area = (0, 0, newWidth, newHeight)
     image = image.crop(area)
-        
+
     return image
 
-def tauxCompression(MCompress,M):
+
+def tauxCompression(MCompress, M):
     nonZeroCompress = np.count_nonzero(MCompress)
     nonZero = np.count_nonzero(M)
-    
+
     return 100-(nonZeroCompress/nonZero)*100
+
 
 def searchBloc(M, i, j):
     """
@@ -43,6 +46,9 @@ def searchBloc(M, i, j):
 
 
 def replaceBloc(M, bloc, i, j):
+    """
+    Remlace le bloc de (i,j) de la matrice M par bloc
+    """
 
     M[8*i:8*(i+1), 8*j:8*(j+1)] = bloc
 
@@ -68,8 +74,6 @@ def decompRGB(imgMatrix):
     Decale aussi les valeurs des pixels pour qu'elles soient entre -128 et 127.
     """
 
-    
-
     centreMatrix = 128*np.ones(imgMatrix.shape)
     imgMatrix = imgMatrix-centreMatrix
 
@@ -80,7 +84,10 @@ def decompRGB(imgMatrix):
     return rMatrix, gMatrix, bMatrix
 
 
-def compression(M):
+def compression(M,Q):
+    """
+    Calcule le spectre de la matrice M et filtre les hautes frequences
+    """
 
     D = np.zeros(M.shape)
 
@@ -99,28 +106,32 @@ def compression(M):
 
     return D
 
-def compdecompression(M):
-    
+
+def compdecompression(M,Q):
+    """
+    Combine compression et decompression(pas utilisee)
+    """
     D = np.zeros(M.shape)
 
     nb_blocsL = M.shape[0]//8
     nb_blocsC = M.shape[1]//8
-    tauxCompression = 0
     for i in range(nb_blocsL):
         for j in range(nb_blocsC):
             bloc = searchBloc(M, i, j)
             bloc = DCT(bloc)
             bloc = (np.divide(bloc, Q))
             bloc = bloc.astype(int)
-            
-             
+
             bloc = np.multiply(bloc, Q)
             replaceBloc(D, invDCT(bloc), i, j)
-    
-    tauxCompression/=(nb_blocsC*nb_blocsL)
+
     return D+np.ones(D.shape)*128
-    
-def decompression(M):
+
+
+def decompression(M,Q):
+    """
+    Decompresse une matrice M
+    """
 
     D = np.zeros(M.shape)
 
@@ -136,82 +147,91 @@ def decompression(M):
     return D+np.ones(D.shape)*128
 
 
-def compressionImg(nomImage):
+def compressionImg(nomImage,Q):
+    """
+    Compresse une image en calculant sa dct et en filtrant les frequences avec la matrice Q
+    """
     # Initialisation
     img = Image.open(nomImage)
     img = tronquer(img)
 
-    imgBase = np.asarray(img)
-    
+    imgBaseMatrix = np.asarray(img)
 
-    r, g, b = decompRGB(imgBase)
-    
-    
-    imgMatrix = np.copy(imgBase)
-    
+    # On recupère les 3 canneaux
+    r, g, b = decompRGB(imgBaseMatrix)
+
+    imgMatrix = np.copy(imgBaseMatrix)
 
     # imgMatrix[:, :, 0] = compdecompression(r)
     # imgMatrix[:, :, 1] = compdecompression(g)
     # imgMatrix[:, :, 2] = compdecompression(b)
 
-    rCompress = compression(r)
-    gCompress = compression(g)
-    bCompress = compression(b)
+    # On calcule les spectres filtrees des canneaux
+    rCompress = compression(r,Q)
+    gCompress = compression(g,Q)
+    bCompress = compression(b,Q)
 
-    tauxCompressionR = tauxCompression(rCompress,r)
-    tauxCompressionG = tauxCompression(gCompress,g)
-    tauxCompressionB = tauxCompression(bCompress,b)
+    # On calcule leurs taux de compression
+    tauxCompressionR = tauxCompression(rCompress, r)
+    tauxCompressionG = tauxCompression(gCompress, g)
+    tauxCompressionB = tauxCompression(bCompress, b)
 
-    tauxCompressionTotal = (tauxCompressionR+tauxCompressionG+tauxCompressionB)/3
+    # On fait la moyenne des taux de compression des 3 canneaux
+    tauxCompressionTotal = (
+        tauxCompressionR+tauxCompressionG+tauxCompressionB)/3
 
-    imgMatrix[:,:,0] = decompression(rCompress)
-    imgMatrix[:,:,1] = decompression(gCompress)
-    imgMatrix[:,:,2] = decompression(bCompress)
+    # On affecte a la matrice de sortie les canneaux comprimes
+    imgMatrix[:, :, 0] = decompression(rCompress,Q)
+    imgMatrix[:, :, 1] = decompression(gCompress,Q)
+    imgMatrix[:, :, 2] = decompression(bCompress,Q)
 
-
+    # On gere les eventueles valeurs qui ne seraient pas entre 0 et 255(erreurs d'arrondi)
     imgMatrix = np.maximum(imgMatrix, 0)
     imgMatrix = np.minimum(imgMatrix, 255)
 
-    
+    # On convertit la matrice en img
     imgCompress = Image.fromarray(imgMatrix)
 
-    return imgCompress,imgMatrix,norm(imgBase-imgMatrix)/norm(imgBase),np.floor((tauxCompressionTotal*100))/100
+    # On renvoie l'image comprimee,sa matrice,l'erreur en norme L2 et le taux de compression de l'image(arrondi au dixieme)
+    return imgCompress, imgMatrix, norm(imgBaseMatrix-imgMatrix)/norm(imgBaseMatrix), np.floor((tauxCompressionTotal*100))/100
 
 
-def fourierImg(nomImage):
+def fourierImg(nomImage,Q):
+    """
+    Calcule le spectre filtree d'une imae
+    """
     # Initialisation
     img = Image.open(nomImage)
     img = tronquer(img)
+    imgBaseMatrix = np.asarray(img)
 
-    imgBase = np.asarray(img)
+    # On recupère les 3 canneaux
+    r, g, b = decompRGB(imgBaseMatrix)
 
-    r, g, b = decompRGB(imgBase)
+    # On copie la matrice de base(bizarrement si on initialise la matrice avec des 0 on obtient des valeurs non entières)
+    imgMatrixF = np.copy(imgBaseMatrix)
 
-    
-    imgMatrixF = np.copy(imgBase)
+    # On affecte a chaque canneaux de la matrice les canneaux comprime
+    imgMatrixF[:, :, 0] = compression(r,Q)
+    imgMatrixF[:, :, 1] = compression(g,Q)
+    imgMatrixF[:, :, 2] = compression(b,Q)
 
-    imgMatrixF[:, :, 0] = compression(r)
-    imgMatrixF[:, :, 1] = compression(g)
-    imgMatrixF[:, :, 2] = compression(b)
-
+    # On gere les eventuels valeurs qui ne seraient pas entre 0 et 255 (erreurs d'arrondi)
     imgMatrixF = np.maximum(imgMatrixF, 0)
     imgMatrixF = np.minimum(imgMatrixF, 255)
 
+    # On convertit la matrice en Image
     imgCompressF = Image.fromarray(imgMatrixF)
 
-    return imgCompressF,imgMatrixF
-
+    return imgCompressF, imgMatrixF
 
 
 if __name__ == '__main__':
-    res = compressionImg("Images\\arouf.jpg")
+    res = compressionImg("Images\\arouf.jpg",Q)
 
     res[0].save("ImageCompressee.jpg")
     print("Difference en norme L2")
     print(res[2])
     print("Taux de compression de l'image")
     print(res[3])
-    fourierImg("Images\\arouf.jpg")[0].save("ImageFourier.jpg")
-
-
-
+    fourierImg("Images\\arouf.jpg",Q)[0].save("ImageFourier.jpg")
--- ProjetPython/script.py
+++ ProjetPython/script.py
@@ -1,21 +1,40 @@
+#! /usr/bin/python3
 from compressionImage import *
 import argparse
 
-parser = argparse.ArgumentParser(description='Compression d image utilisant la dct')
+parser = argparse.ArgumentParser(
+    description='Compression d image utilisant la dct')
 
-parser.add_argument('ifile',help='chemin de l image de depart')
-parser.add_argument('ofile',help='chemin de l image comprimee')
-parser.add_argument('-f','--fourier',action="store_true",help="Option qui permet de sauvegarder le spectre apres compression de l'image")
-parser.add_argument('-i','--info',action="store_true",help="Option qui permet d'afficher le taux de compression de l'image et l'erreur en norme L2")
-
+parser.add_argument('ifile', help='chemin de l image de depart')
+parser.add_argument('ofile', help='chemin de l image comprimee')
+parser.add_argument('-f', '--fourier', action="store_true",
+                    help="Option qui permet de sauvegarder le spectre apres compression de l'image")
+parser.add_argument('-i', '--info', action="store_true",
+                    help="Option qui permet d'afficher le taux de compression de l'image,l'erreur en norme L2 et la matrice Q utilisee")
+parser.add_argument('-q', '--quantization', help="Permet de choisir le matrice de quantification,l'option 0 choisit la matrice de jpg, et les options de 1 à 8 mettent Q avec que des 255 sauf le bloc en haut à gauche de la taille choisie qui contient des 1.", default=0, type=int)
 
 args = parser.parse_args()
 
-res = compressionImg(args.ifile)
+
+#On construit la matrice Q
+if(args.quantization >= 1 and args.quantization <= 8):
+    i = args.quantization
+    Q = np.ones((8, 8))*1000
+    Q[0:i,0:i] = 1
+else:
+    #Par défaut on utilise la matrice de jpg
+    Q = np.matrix('16 11 10 16 24 40 51 61;12 12 13 19 26 58 60 55;14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62;18 22 37 56 68 109 103 77;24 35 55 64 81 104 113 92;49 64 78 87 103 121 120 101;72 92 95 98 112 100 103 99')
+
+    
+
+#On compresse l'image choisie
+res = compressionImg(args.ifile,Q)
+#On la sauvegarde a l'endroit choisi
 res[0].save(args.ofile)
 
+#Si on a mis l'option -f on sauvegarde le spectre de l'image au meme endroit que l'image compresee avec "Fourier" avant l'extension
 if args.fourier:
-    imgCompressFourier = fourierImg(args.ifile)[0]
+    imgCompressFourier = fourierImg(args.ifile, Q)[0]
     split = args.ofile.split(".")
     split[-2] += "Fourier"
     outPutFourier = "."
@@ -23,9 +42,11 @@ if args.fourier:
     # print(outPutFourier)
     imgCompressFourier.save(outPutFourier)
 
+#On affiche les informations si on a mis l'option -i
 if args.info:
     print("Difference en norme L2")
     print(res[2])
     print("Taux de compression de l'image")
     print(res[3])
-
+    print("Matrice de quantification utilisee")
+    print(Q)
