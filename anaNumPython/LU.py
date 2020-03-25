91cf72ded385754dfbdd67c29fa46270bda05643
Isnard <enzoisnard@gmail.com>
2020-03-25 12:39:27 +0100

    mise a jour de la facto LU

--- anaNumPython/LU.py
+++ anaNumPython/LU.py
@@ -8,7 +8,7 @@ def factLU(A):
     """
     n = A.shape[1]
     U = A.copy()
-    L = np.identity(n)
+    L = np.eye(n)
 
     for k in range(0, n-1):
         for i in range(k + 1, n):
@@ -73,20 +73,37 @@ def factLUP(A):
     """
     Factorise une matrice A en la forme PA=LU ou L est triangulaire inférieure ,U triangulaire supérieure et P une matrice de permutation
     """
-    n = A.shape[1]
-    U = A.copy()
-    L = np.identity(n)
-    P = np.identity(n)
+    U = np.copy(A)
+    N = U.shape
+    P = np.eye(N[0])
+    L = np.eye(N[0])
+    for k in range(0, N[0]-1):
+        #calcul de l'indice du plus grand pivot
+        ind = np.argmax(abs(U[k:N[0], k]))
 
-    for k in range(0, n - 1):
-        k0 = np.argmax(abs(U[k:n, k]))+k-1
-        P[[k, k0]] = P[[k0, k]]
-        U[[k, k0]] = U[[k0, k]]
-
-        for i in range(k + 1, n):
-            alpha = U[i, k] / U[k, k]
-            U[i, :] = U[i, :] - alpha * U[k, :]
-            L[i, k] = alpha
+        ind = ind+k
+        #calcul de la matrice de permutation utilisée à l'étape k et échange des lignes k et ind dans la matrice U
+        Ptmp = np.eye(N[0])
+        if(ind != k):
+            Ptmp[ind, k] = 1.
+            Ptmp[ind, ind] = 0.
+            Ptmp[k, ind] = 1.
+            Ptmp[k, k] = 0.
+            # Mise à jour de la matrice de permutation globale
+            P = np.dot(Ptmp, P)
+            u = np.copy(U[ind, k:N[0]])
+            U[ind, k:N[0]] = U[k, k:N[0]]
+            U[k, k:N[0]] = u
+        #Combinaison linéaire pour mettre des O sur la colonne k et construction de la matrice M (équivalent de la matrice L mais pour l'étape k uniquement)
+        M = np.eye(N[0])
+        for j in range(k+1, N[0]):
+            alpha = U[j, k]/U[k, k]
+            U[j, :] = U[j, :]-alpha*U[k, :]
+            M[j, k] = alpha
+        #Mise à jour de la matrice L
+        L = np.dot(L, np.dot(np.transpose(Ptmp), M))
+    # Permutation de la matrice L
+    L = np.dot(P, L)
 
     return L, U, P
 
@@ -106,10 +123,37 @@ def factLUP(A):
 # print(lin.inv(A)*b)
 
 A = np.array([[1, 1 + 0.5E-15, 3], [2, 2, 20], [3, 6, 4]])
-print(A)
-L,U = factLU(A)
+L, U = factLU(A)
 print(np.dot(L, U))
 
 L2, U2, P = factLUP(A)
-print(np.dot(L, U))
-print(P)
+print(np.dot(P,np.dot(L2,U2)))
+
+
+# ϵ = 1E-13
+# b = np.zeros((2, 1))
+# b[0] = 1 + ϵ
+# b[1]=2
+# A = np.array([[ϵ,1],[1,1]])
+
+# L, U = factLU(A)
+
+# x = solveLinSys(L, U, b)
+# print(x)
+
+# L2, U2, P = factLUP(A)
+# x2 = solveLinSysP(L2, U2, P, b)
+# print(P)
+# print(x2)
+
+# e = 1E-14
+# A = np.array([[e, 1, 1], [1, 1, -1], [1, 1, 2]])
+# b = np.array([[1,-1,1]]).T
+
+# L, U = factLU(A)
+# x = solveLinSys(L, U, b)
+# print(x,end="\n")
+# print(np.dot(lin.inv(A),b))
+# L2, U2, P = factLUP(A)
+# x2 = solveLinSysP(L, U, P, b)
+# print(x2)
