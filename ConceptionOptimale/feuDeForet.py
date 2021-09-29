39fec67eee493a071be5869f0599359d9313b813
Enzo Isnard <enzoisnard@gmail.com>
2021-09-29 22:10:44 +0200

    cleaned project

--- ConceptionOptimale/feuDeForet.py
+++ ConceptionOptimale/feuDeForet.py
@@ -7,7 +7,7 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
+from time import perf_counter
 
 def progress(i, n):
     i += 1
@@ -15,17 +15,15 @@ def progress(i, n):
     print(f'\rProgression:[{k*"#"}{(20-k)*" "}] [{(i/n*100):.0f} %]',
           end='' if i != n else "\n", flush=True)
 
-
 def randomC(n_circ, a, b):
     R = np.zeros((ns, ns))
     for _ in range(n_circ):
-        x, y, r, v = rand(), rand(), rand()*0.2, (b-a)*rand()+a
+        x, y, r, v = rand(), rand(), rand()*0.3, (b-a)*rand()+a
         R += ((X-x)**2+(Y-y)**2 <= r**2)*v
     return R
 
-
-ns = 100
-nt = 100
+ns = 101
+nt = 550
 mu = 5e-3
 delta = 1/ns
 x = np.linspace(0, 1, ns)
@@ -34,19 +32,14 @@ X, Y = np.meshgrid(x, y)
 u = np.cos(np.pi*Y)
 v = 0.6*np.sin(np.pi/2*X)
 V = np.array([u, v])
-c = np.max(u**2+v**2)
+c = np.max((u**2+v**2)**(1/2))
 dt = 1/8*min(delta**2/(mu), c/delta)
 # plt.quiver(X, Y, V[0], V[1], units="xy")
 # plt.show()
-
+print(f"{dt = }")
 T = np.zeros((nt, ns, ns))
 C = np.zeros((nt, ns, ns))
-C[0] = 5+randomC(10, -5, 5)
-# plt.contourf(X, Y, C[0], cmap="Greens")
-# plt.colorbar()
-# plt.show()
-# print(C[0])
-# T[0,10:30,10:30] = 100
+C[0] = 5+randomC(3, -5, 5)
 T[0] = (((X-0.1)**2+(Y-0.1)**2) <= 0.001)*5
 
 
@@ -63,40 +56,23 @@ def gradT(n):
 
     return grad
 
-def gradT2(n,i,j):
-    grad = np.zeros((2,))
-    if V[0,i,j]>=0:
-        grad[1] = (T[n,i,j]-T[n,i-1,j])/delta
-    else:
-        grad[1] = (T[n,i+1,j]-T[n,i,j])/delta
-    if V[1,i,j]>=0:
-        grad[0] = (T[n,i,j]-T[n,i,j-1])/delta
-    else:
-        grad[0] = (T[n,i,j+1]-T[n,i,j])/delta
-    return grad
-
-
-
-
 def laplacienT(n):
     return (T[n, 2:, 1:-1]+T[n, :-2, 1:-1]+T[n, 1:-1, 2:]+T[n, 1:-1, :-2]-4*T[n, 1:-1, 1:-1])/delta**2
 
-
 @np.vectorize
 def R(T, C):
-    if T >= 0.05 and C > 0:
+    if T >= 0.05 and C >= 0:
         return 10
-    elif T >= 0.05 and C <= 0:
+    elif T >= 0.05 and C < 0:
         return -5
     elif T < 0.05:
         return 0
-
 
 @np.vectorize
 def R2(T):
     return int(T > 0.05)*(-60)
 
-
+start = perf_counter()
 for n in range(nt-1):
     progress(n, nt-1)
     T[n+1,1:-1,1:-1] = T[n,1:-1,1:-1]+dt*(mu*laplacienT(n)-np.einsum("ijk,ijk->jk",V[:,1:-1,1:-1],gradT(n))+R(T[n,1:-1,1:-1],C[n,1:-1,1:-1])*T[n,1:-1,1:-1])
@@ -104,25 +80,12 @@ for n in range(nt-1):
     T[n+1, :, -1] = T[n+1, :, -2]
     T[n+1, 0, :] = T[n+1, 1, :]
     T[n+1, -1, :] = T[n+1, -2, :]
-    C[n+1, :, :] = C[n, :, :]+dt*R2(T[n, :, :])*C[n, :, :]
-
-# for n in range(nt-1):
-#     progress(n, nt-1)
-#     for i in range(1, ns-1):
-#         for j in range(1, ns-1):
-#             T[n+1, i, j] = T[n, i, j]+dt * \
-#                 (mu*(T[n, i+1, j]+T[n, i-1, j]+T[n, i, j+1] +
-#                  T[n, i, j-1]-4*T[n, i, j])/delta**2-V[:,i,j]@gradT2(n,i,j)+R(T[n,i,j],C[n,i,j])*T[n,i,j])
-    
-#     T[n+1, :, 0] = T[n+1, :, 1]
-#     T[n+1, :, -1] = T[n+1, :, -2]
-#     T[n+1, 0, :] = T[n+1, 1, :]
-#     T[n+1, -1, :] = T[n+1, -2, :]
-#     C[n+1,:,:] = C[n,:,:]+dt*R2(T[n,:,:])*C[n,:,:]
+    C[n+1, :, :] = C[n, :, :]+dt*R2(T[n])
+print(f"Time taken: {perf_counter()-start:.2f} s")
 
 fig, axs = plt.subplots(ncols=2)
 norm = mpl.colors.Normalize(vmin=0, vmax=5)
-norm2 = mpl.colors.Normalize(vmin=0,vmax=5)
+norm2 = mpl.colors.Normalize(vmin=0,vmax=10)
 
 im1 = axs[0].imshow(T[0], cmap='jet', norm=norm,extent=[0,1,1,0])
 axs[0].invert_yaxis()
@@ -132,17 +95,15 @@ im2 = axs[1].imshow(C[0],cmap='Greens',norm=norm2,extent=[0,1,1,0])
 axs[1].invert_yaxis()
 plt.colorbar(im2, pad=0.1,ax=axs[1])
 
-
 def animate(i):
     Ti = T[i]
     Ci = C[i]
     im1.set_data(Ti)
     im2.set_data(Ci)
     axs[0].set_title(f"Frame {i}")
-    # plt.title(f"{i}")
 
-
-animation = FuncAnimation(fig, animate, frames=nt-1)
+animation = FuncAnimation(fig, animate, frames=nt-1,interval=1)
 
 plt.show()
-print(T[-1])
+
+print(np.max(T[-1]))
