7b0c11133f3682015eef3d05e21f383ea423dc28
Enzo Isnard <enzoisnard@gmail.com>
2021-11-01 15:19:29 +0100

    .

--- ValorisationDesDonnes/Sachito.R
+++ ValorisationDesDonnes/Sachito.R
@@ -40,8 +40,8 @@ fig <- eth %>% plot_ly(x = ~eth$Date, type="candlestick",
                        increasing = i, decreasing = d)
 fig
 
-index = 1845
-btc_ts = ts(btc$Close,start = c(2013,4,29),frequency = 1)
+index = 1800
+btc_ts = ts(btc$Close,frequency = 1)
 btc_ts2 = ts(btc_ts[1:index])
 autoplot(btc_ts2,type="l")
 btc_diff2 = diff(btc_ts2)
@@ -50,15 +50,13 @@ plot(btc_diff2,type="l")
 model = auto.arima(btc_ts2,approximation = FALSE,stepwise = FALSE,seasonal = TRUE)
 summary(model)
 checkresiduals(model)
-h_pred=28
-forecast = forecast(model,h=h_pred)
-autoplot(forecast,include = 100)
+h_pred=7
+forecast1 = forecast(model,h=h_pred)
+autoplot(forecast1,include = 100)
 
-forecast = sarima.for(btc_ts2,h_pred,5,1,0)
 
-df = ts_data.frame(forecast$pred)
+df = ts_data.frame(forecast1$mean)
 df$true = btc_ts[index:(index+h_pred-1)]
-df$se = forecast$se
 df
 fig <- plot_ly(df, x = ~time, y = ~value, name = 'prediction', type = 'scatter', mode = 'lines',color=I("red"))
 fig <- fig %>% add_trace(y = ~value+se, name = 'max', mode = 'lines',color = I("black"))
